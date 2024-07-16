42b838179351df921dcd9da008aa8e2f58a50cb7
Hemant Gupta <92354762+HkgGupta@users.noreply.github.com>
2024-07-16 13:52:32 +0530

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://notebox-api.onrender.com/api/user',
-    timeout: 5000,
+    timeout: 15000,
     headers: {
         'Content-Type': 'application/json',
     }
